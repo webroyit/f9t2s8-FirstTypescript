d7badb9a4f5f39c6808c7c1fd285bb91e606f1ae
royweb <xytank@gmail.com>
2020-10-04 13:36:21 -0400

    Examples of more types in typescript

--- src/components/Page1.tsx
+++ src/components/Page1.tsx
@@ -7,12 +7,23 @@ class Page1 extends Component{
     //let firstValue: number = 34;
     //let firstValue: boolean = true;
     //let firstValue: number[] = [1, 2, 3];
-    let firstValue: Array<string> = ['dog', '7', 'house'];
+    //let firstValue: Array<string> = ['dog', '7', 'house'];
+
+    // enum is similar to an object
+    enum Foods {watermelon = 1, berry = 2};
+
+    // any if you are not sure which type to use
+    let firstName: any = 'Bob';
+
+    // void if you are returning anything
+    const test = (): void => {
+      console.log("test");
+    }
 
     return(
       <div>
         <p>
-          The value {firstValue} is of {typeof firstValue} type
+          The value {firstName} is of {typeof firstName} type
         </p>
       </div>
     )
