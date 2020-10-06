f6f5fefd38c2a97b69bc296c686b436746884266
royweb <xytank@gmail.com>
2020-10-06 13:37:08 -0400

    Example of using interface

--- src/components/Page2.tsx
+++ src/components/Page2.tsx
@@ -2,12 +2,12 @@ import React, { Component } from 'react';
 
 import Part1 from './Part1';
 
-class Page2 extends Component{
+class Page2 extends Component<any>{
   render(){
 
     return(
       <div>
-        <Part1 text="How are you?" />
+        <Part1 name="Bob" text="how are you?" />
       </div>
     )
   }
--- src/components/Part1.tsx
+++ src/components/Part1.tsx
@@ -1,9 +1,16 @@
 import React from 'react';
 
+// interface check for types and valiadate
+// it does not work in javascript
+interface SomeData{
+    name: string;
+    text: string;
+}
+
 // By default, the return type is any
-const Part1 = (prop: {text: string}): any => {
+const Part1 = (props: SomeData): any => {
     return(
-        <p>{prop.text}</p>
+        <p>{props.name}, {props.text}</p>
     );
 }
 
